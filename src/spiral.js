3f822521be47d2675f3a9d11b5c41b8960306550
nazeh <ar.nazeh@gmail.com>
2020-04-07 00:03:39 +0300

    Add eslint

--- src/spiral.js
+++ src/spiral.js
@@ -1,27 +1,29 @@
 // Props
-const SIDE_LENGTH = 400;
+const WIDTH = 600;
+const HEIGHT = 600;
 const RADIAL_STEPS = 10; // Even & > 8
 const POLAR_TURNS = 50;
 
 // derived
-const diagonalLength = Math.sqrt(Math.pow(SIDE_LENGTH, 2) * 2);
+const diagonalLength = Math.sqrt(WIDTH ** 2 + HEIGHT ** 2);
 const polarGap = diagonalLength / (POLAR_TURNS * 12); // 15 is eyeballed
-const center = SIDE_LENGTH / 2;
+const centerX = WIDTH / 2;
+const centerY = (HEIGHT || WIDTH) / 2;
 const increment = (2 * Math.PI) / RADIAL_STEPS;
 
 // Setup
 const c = document.getElementById('canvas');
-c.width = SIDE_LENGTH;
-c.height = SIDE_LENGTH;
+c.width = WIDTH;
+c.height = HEIGHT;
 const ctx = c.getContext('2d');
-ctx.moveTo(center, center);
+ctx.moveTo(centerX, centerY);
 
 let angle = increment;
 
 while (angle < POLAR_TURNS * 2 * Math.PI) {
-  let x = center + angle * Math.cos(angle) * polarGap;
-  let y = center + angle * Math.sin(angle) * polarGap;
+  const x = centerX + angle * Math.cos(angle) * polarGap;
+  const y = centerY + angle * Math.sin(angle) * polarGap;
   ctx.lineTo(x, y);
-  angle = angle + increment;
+  angle += increment;
 }
 ctx.stroke();
