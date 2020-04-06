c72a1ddfba1f4d1027e645cfa38032d3ec436c63
nazeh <ar.nazeh@gmail.com>
2020-04-07 00:41:31 +0300

    Control number of turns with RESOLUTION

--- src/spiral.js
+++ src/spiral.js
@@ -1,15 +1,18 @@
 // Props
-const WIDTH = 600;
-const HEIGHT = 600;
-const RADIAL_STEPS = 10; // Even & > 8
-const POLAR_TURNS = 50;
+const WIDTH = 600; // px
+let HEIGHT = null; // px, null if square
+const RESOLUTION = 72;
+
+const polarSteps = RESOLUTION / 8; // Even & > 8
+const POLAR_TURNS = RESOLUTION * 6; // eyeballed 6
 
 // derived
+HEIGHT = HEIGHT || WIDTH;
 const diagonalLength = Math.sqrt(WIDTH ** 2 + HEIGHT ** 2);
-const polarGap = diagonalLength / (POLAR_TURNS * 12); // 15 is eyeballed
+const polarGap = diagonalLength / POLAR_TURNS;
 const centerX = WIDTH / 2;
-const centerY = (HEIGHT || WIDTH) / 2;
-const increment = (2 * Math.PI) / RADIAL_STEPS;
+const centerY = HEIGHT / 2;
+const increment = (2 * Math.PI) / polarSteps;
 
 // Setup
 const c = document.getElementById('canvas');
@@ -20,7 +23,7 @@ ctx.moveTo(centerX, centerY);
 
 let angle = increment;
 
-while (angle < POLAR_TURNS * 2 * Math.PI) {
+while (angle < POLAR_TURNS * Math.PI) {
   const x = centerX + angle * Math.cos(angle) * polarGap;
   const y = centerY + angle * Math.sin(angle) * polarGap;
   ctx.lineTo(x, y);
