43a534f8bac90ffc948460621f361314a889e3a4
nazeh <ar.nazeh@gmail.com>
2020-04-11 13:30:00 +0300

    Refactoring the setup script

--- src/scripts/index.ts
+++ src/scripts/index.ts
@@ -1,62 +1,52 @@
 import Spiral from "../spiral";
 
 const SingleStroke = (() => {
-  let img;
-  let canvas;
-  let ctx;
-  let spiral;
-  let imgValues = [];
-  let width;
-  let height;
+  const setup = ({ canvas, img }) => {
+    const host = canvas.parentElement;
 
-  const setup = () => {
-    canvas.width = canvas.parentElement.offsetWidth;
-    canvas.height = (canvas.width * img.height) / img.width;
+    canvas.width = host.clientWidth;
+    canvas.height = (host.clientWidth * img.height) / img.width;
 
-    [width, height] = [canvas.width, canvas.height];
-    ctx = canvas.getContext("2d");
+    const [width, height] = [canvas.width, canvas.height];
+
+    return [canvas.getContext("2d"), width, height];
   };
 
-  const setBaseSpiral = () => {
-    const diagonal = Math.sqrt(canvas.width ** 2 + canvas.height ** 2);
+  const setBaseSpiral = ({ width, height }) => {
+    const diagonal = Math.sqrt(width ** 2 + height ** 2);
 
-    spiral = Spiral({
-      center: { x: canvas.width / 2, y: canvas.height / 2 },
+    return Spiral({
+      center: { x: width / 2, y: height / 2 },
       diameter: diagonal,
     });
-
-    getImageValues();
   };
 
-  const getImageValues = () => {
+  const getImageValues = ({ canvas, img, spiral }) => {
     const imgCanvas = document.createElement("canvas");
-    imgCanvas.width = width;
-    imgCanvas.height = height;
-    const imgCtx = imgCanvas.getContext("2d");
-    imgCtx.drawImage(img, 0, 0, width, height);
+    imgCanvas.width = canvas.width;
+    imgCanvas.height = canvas.height;
+    const ctx = imgCanvas.getContext("2d");
+    ctx.drawImage(img, 0, 0, imgCanvas.width, imgCanvas.height);
 
-    imgValues = spiral.points.map((point) => {
+    return spiral.points.map((point) => {
       const { x, y } = point;
-      const rgb = imgCtx.getImageData(x, y, 1, 1).data;
+      const rgb = ctx.getImageData(x, y, 1, 1).data;
       return (rgb[0] + rgb[1] + rgb[2]) / 3 / 255;
     });
-
-    return imgValues;
   };
 
-  const draw = () => {
+  const draw = ({ ctx, spiral, imgValues }) => {
     spiral.displace(imgValues, 10);
     spiral.points.forEach((point) => {
       ctx.lineTo(point.x, point.y);
     });
   };
 
-  const run = (_canvas, _img) => {
-    canvas = _canvas;
-    img = _img;
-    setup();
-    setBaseSpiral();
-    draw();
+  const run = (canvas, img) => {
+    const [ctx, width, height] = setup({ canvas, img });
+    const spiral = setBaseSpiral({ width, height });
+    let imgValues = getImageValues({ canvas, img, spiral });
+    draw({ ctx, spiral, imgValues });
     // ctx.closePath();
     // ctx.fill();
     ctx.stroke();
