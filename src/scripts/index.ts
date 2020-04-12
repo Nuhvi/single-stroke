f3c7a65c0f13918f0b0b1811e4e13613e9abe3cd
nazeh <ar.nazeh@gmail.com>
2020-04-12 13:19:01 +0300

    Add animation function to animate drawing

--- src/scripts/index.ts
+++ src/scripts/index.ts
@@ -36,25 +36,69 @@ const SingleStroke = (() => {
     });
   };
 
-  const draw = ({ ctx, spiral, imgValues }) => {
+  const drawSegment = (ctx, points, halfLen, lastStep, nextStep) => {
+    ctx.moveTo(
+      points[halfLen + lastStep - 1].x,
+      points[halfLen + lastStep - 1].y
+    );
+
+    for (let i = lastStep; i <= nextStep; i++) {
+      ctx.lineTo(points[halfLen + i].x, points[halfLen + i].y);
+    }
+    for (let i = nextStep; i >= lastStep; i--) {
+      ctx.lineTo(points[halfLen - i].x, points[halfLen - i].y);
+    }
+
+    ctx.closePath();
+    ctx.fill();
+  };
+
+  const animate = (
+    ctx,
+    points,
+    halfLen,
+    lastStep = 0,
+    nextStep = 0,
+    accelerating = 0.1
+  ) => {
+    drawSegment(ctx, points, halfLen, lastStep, nextStep);
+
+    lastStep = nextStep;
+    nextStep += Math.ceil(lastStep * accelerating + 1);
+
+    if (nextStep < halfLen) {
+      requestAnimationFrame(() => {
+        animate(ctx, points, halfLen, lastStep, nextStep, accelerating);
+      });
+    } else {
+      drawSegment(ctx, points, halfLen, lastStep, halfLen - 1);
+    }
+  };
+
+  const draw = ({ spiral, imgValues }) => {
     const map = imgValues;
     spiral
       .displace({ map: blur(imgValues, 5), strength: 10 })
-      .createStroke({ map, thickness: 10 })
-      .points.forEach((point) => {
-        ctx.lineTo(point.x, point.y);
-      });
+      .createStroke({ map, thickness: 10 });
   };
 
   return {
     run: (canvas, img) => {
+      console.time("setup");
       const [ctx, width, height] = setup({ canvas, img });
+      console.timeEnd("setup");
+      console.time("create spiral");
       const spiral = setBaseSpiral({ width, height });
+      console.timeEnd("create spiral");
+      console.time("read Image data");
       let imgValues = getImageValues({ canvas, img, spiral });
-      draw({ ctx, spiral, imgValues });
-      ctx.closePath();
-      ctx.fill();
-      // ctx.stroke();
+      console.timeEnd("read Image data");
+      console.time("modify spiral");
+      draw({ spiral, imgValues });
+      console.timeEnd("modify spiral");
+      console.time("draw canvas");
+      animate(ctx, spiral.points, Math.floor(spiral.points.length / 2));
+      console.timeEnd("draw canvas");
     },
   };
 })();
