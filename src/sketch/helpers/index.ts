cd4dd7eabadf5a7d1f05b0d18e354b011b64aca3
nazeh <ar.nazeh@gmail.com>
2020-04-22 14:11:07 +0300

    Refactor scaleNeeded()

--- src/sketch/helpers/index.ts
+++ src/sketch/helpers/index.ts
@@ -2,15 +2,8 @@ export const scaleNeeded = (
   src: { w: number; h: number },
   trgt: { w: number; h: number },
 ): number => {
-  let scale = 1;
-  const srcAspectRatio = src.w / src.h;
-  const trgtAspectRatio = trgt.w / trgt.h;
+  const wr = trgt.w / src.w;
+  const hr = trgt.h / src.h;
 
-  if (srcAspectRatio >= trgtAspectRatio) {
-    scale = trgt.w / src.w;
-  } else {
-    scale = trgt.h / src.h;
-  }
-
-  return scale;
+  return Math.min(wr, hr);
 };
