805c0428bbecaee64ddc94713aceb5f75ae4cd7d
nazeh <ar.nazeh@gmail.com>
2020-04-24 11:15:39 +0300

    Remove legacy code

--- legacy/src/index.ts
+++
@@ -1,9 +0,0 @@
-import UI from './view/index';
-import './style.css';
-import Controller from './controller/index';
-
-const view = UI({
-  el: 'body',
-});
-
-Controller(view);
--- legacy/src/sketch/helpers/index.ts
+++
@@ -1,9 +0,0 @@
-export const scaleNeeded = (
-  src: { w: number; h: number },
-  trgt: { w: number; h: number },
-): number => {
-  const wr = trgt.w / src.w;
-  const hr = trgt.h / src.h;
-
-  return Math.min(wr, hr);
-};
